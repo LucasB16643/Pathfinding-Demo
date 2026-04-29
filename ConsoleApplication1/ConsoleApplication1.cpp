3c44b7f63fb85441b39ee3c3af0b7dd4a776a1f5
LucasB16643 <144140730+LucasB16643@users.noreply.github.com>
2026-04-29 11:11:29 -0400

    remove outdated code

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,5 +1,4 @@
-﻿// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+﻿
 
 #include <iostream>
 #include "raylib.h"
@@ -8,7 +7,7 @@
 #include "IPathfinder.h"
 #include "BFS.h"
 
-//void CreatePathfinder(Grid& grid, Node* start, Node* target);
+
 
 enum class Algorithm
 {
@@ -54,7 +53,7 @@ int main()
     Node* target = &grid.nodes[19][19];
 
     std::vector<Node*> path;
-    //Pathfinder* pathfinder = nullptr;
+    
 
     Node* draggedNode = nullptr;
     bool isDraggingStart = false;
@@ -214,13 +213,4 @@ int main()
    
 
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
